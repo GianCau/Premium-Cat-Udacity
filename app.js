3bb2b45d353ab185bb3c5fdbd5efe80d4a14bbf0
gcausero <gcausero@comwrap.com>
2019-03-31 11:07:57 +0200

    fixed issue

--- app.js
+++ app.js
@@ -1,82 +1,6 @@
-const count = document.querySelector('#count');
 
-let counter = 0;
-
-
-let catObject = [
-	{
-		name: 'Miao',
-		img: 'img/cat.jpg',
-		click: 0,
-		show: true
-	},
-	{
-		name: 'Ade',
-		img: 'img/cat2.jpg',
-		click: 3,
-		show: false
-	},
-	{
-		name: 'Zeus',
-		img: 'img/cat3.jpg',
-		click: 0,
-		show: false
-	},
-	{
-		name: 'Rusty',
-		img: 'img/cat4.jpg',
-		click: 4,
-		show: false
-	},
-	{
-		name: 'Katty',
-		img: 'img/cat5.jpg',
-		click: 0,
-		show: false
-	},
-]
-
-// cats.forEach((item,index,)=> {
-
-// 	item.addEventListener('click', function(){
-
-
-// 		//show(index)
-
-
-// 	}, false);
-// })
-
-//give the output to show the values
-function show(pos){
-
-	name.innerHTML = catObject[pos].name;
-	count.innerHTML = catObject[pos].click;
-	remove()
-	showImg(catObject[pos].img)
-}
-
-
-
-//show the image
-function showImg(par) {
-	var img = document.createElement("img");
-	img.style.width = "300px";
-	img.style.height = "300px";
-
-	//img.src = par;
-	img.style.backgroundImage= `url(${par})`;
-	document.getElementById("imageContainer").appendChild(img);
-}
-
-//dovro creare un image object
-
-
-
-	let modal = {
-		init: function () {
-
-		},
+	const modal = {
+		currentCat: null,
 		cats: [
 			{
 				name: 'Miao',
@@ -87,7 +11,7 @@ function showImg(par) {
 			{
 				name: 'Ade',
 				img: 'img/cat2.jpg',
-				click: 3,
+				click: 0,
 				show: false
 			},
 			{
@@ -99,7 +23,7 @@ function showImg(par) {
 			{
 				name: 'Rusty',
 				img: 'img/cat4.jpg',
-				click: 4,
+				click: 0,
 				show: false
 			},
 			{
@@ -115,119 +39,89 @@ function showImg(par) {
 
 
 
-
-	let octopus = {
+	const octopus = {
 		init: function() {
-				//model.init();
 
-				view.init();
+			modal.currentCat = modal.cats[0];
+
+			cat_view.init()
+			view.init()
+
 			},
-		currentCat : function(num) {
+		getCurrentCat : function() {
 
-			 console.log(modal.cats[num]);
+			this.cat = modal.currentCat
+			return this.cat
 
+		},
+		setCurrenCat: function (e) {
+			modal.currentCat = modal.cats[e];
+
+		},
+		catData: function () {
+
+			return modal.cats[this.cat].name
+
+		},
+		clickData: function () {
+
+			return modal.cats[this.cat].click
+		},
+		incrementCounter: function () {
+
+			modal.currentCat.click++
+			view.render()
 		}
-
 	};
+
+	const cat_view = {
+
+		init: function () {
+
+			let catImg = document.getElementById('catImg');
+			const currentCat = octopus.getCurrentCat()
+
+			catImg.addEventListener('click', function () {
+
+				octopus.incrementCounter()
+
+			})
+		}
+	}
 
 	let view = {
 
 		init: function() {
 
-			const cats = document.querySelectorAll('li');
+			this.elements = document.querySelectorAll('li')
 
-			cats.forEach((item,index)=> {
+			this.elements.forEach((item,index) => {
 
-				item.addEventListener('click', function(){
+				item.addEventListener('click', function(e){
 
-
-					octopus.currentCat(index)
+					octopus.setCurrenCat(index)
+					view.render()
 
 				}, false);
-			})
 
-			view.render()
+			});
+
+
+			this.render()
 		},
-		render = function() {
 
-			const name = document.querySelector('#name')
+		render: function() {
+
+			const currentCat = octopus.getCurrentCat()
+			const idName = document.getElementById('name')
+			const idClick = document.getElementById('count')
+			const catImg = document.getElementById('catImg')
+
+
+			idName.innerHTML = currentCat.name;
+			idClick.innerHTML = currentCat.click;
+			catImg.src = currentCat.img
 		}
-
 	};
-
-
-
-
 // make it go!
 octopus.init();
-
-
-
-// $(function(){
-
-//     var model = {
-//         init: function() {
-//             if (!localStorage.notes) {
-//                 localStorage.notes = JSON.stringify([]);
-//             }
-//         },
-//         add: function(obj) {
-//             console.log(obj);
-
-//             var data = JSON.parse(localStorage.notes);
-//             data.push(obj);
-//             localStorage.notes = JSON.stringify(data);
-//         },
-//         getAllNotes: function() {
-//             return JSON.parse(localStorage.notes);
-//         }
-//     };
-
-
-//     var octopus = {
-//         addNewNote: function(noteStr) {
-
-//             model.add({
-//                 content: noteStr,
-//                 date: Date.now()
-//             });
-//             view.render();
-//         },
-
-//         getNotes: function() {
-//             return model.getAllNotes();
-//         },
-
-//         init: function() {
-//             model.init();
-//             view.init();
-//         }
-//     };
-
-
-//     var view = {
-//         init: function() {
-//             this.noteList = $('#notes');
-//             var newNoteForm = $('#new-note-form');
-//             var newNoteContent = $('#new-note-content');
-//             newNoteForm.submit(function(e){
-//                 octopus.addNewNote(newNoteContent.val());
-//                 newNoteContent.val('');
-//                 e.preventDefault();
-//             });
-//             view.render();
-//         },
-//         render: function(){
-//             var htmlStr = '';
-//             octopus.getNotes().forEach(function(note){
-
-//                 htmlStr += `<li class="note">
-//                         ${note.content} ${note.content}
-//                     </li>`;
-//             });
-//             this.noteList.html( htmlStr );
-//         }
-//     };
-
-//     octopus.init();
-// });
